8405feabdf63961bd82bc8c0d47bc8604892d837
Carlos J. Argüello <cjarguello@users.noreply.github.com>
2017-05-12 17:34:24 -0700

    Create worker.js

---
+++ js/worker.js
@@ -0,0 +1,23 @@
+$(function() {
+  setTimeout(replaceTitles, 5000);
+});
+
+function replaceTitles() {
+  $.each(['Users', 'Admins', 'Newbis'], function(idx, val) {
+    replaceTitle(val, '.story_name');
+  });
+
+  $.each(['AK', 'HK', 'CJ'], function(i, val) {
+    replaceTitle(val, '.owner');
+  })
+}
+
+function replaceTitle(name, cssClass) {
+  var imgTag = "<img src='" + chrome.extension.getURL('images/' + name.toLowerCase() + '.png') + "'/>";
+
+  $(cssClass + ":contains('" + name + "'):visible").each(function() {
+    var new_content = $(this).text()
+                             .replace(name, imgTag);
+    $(this).html(new_content);
+  });
+}

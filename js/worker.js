81b8d75007db0fe240df4a808cf9f12f153cf902
Artem Kozaev <artem@gmx.ie>
2017-05-14 14:26:09 +0300

    Remove redundant jquery lib, add case insensitive matcher, add css classes to stories

--- js/worker.js
+++ js/worker.js
@@ -1,23 +1,38 @@
 $(function() {
+  jQuery.expr[":"].containsCaseInsensitive = jQuery.expr.createPseudo(function(arg) {
+     return function(elem) {
+       return jQuery(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
+     };
+  });
+
   setTimeout(replaceTitles, 5000);
 });
 
 function replaceTitles() {
-  $.each(['Quick', 'Admins', 'Newbis'], function(idx, val) {
-    replaceTitle(val, '.story_name');
-  });
+  var TO_REPLACE = {
+    '.story_name': ['Quick', 'Admins', 'Newbis'],
+    '.owner': ['AK', 'CJ']
+  }
 
-  $.each(['AK', 'CJ'], function(i, val) {
-    replaceTitle(val, '.owner');
-  })
+  Object.keys(TO_REPLACE).forEach(function(cssClass) { // loop thru keys in TO_REPLACE; key = cssClass
+    TO_REPLACE[cssClass].forEach(function(text) { // loop thru values in TO_REPLACE; values are texts to replace
+      replaceTitle(text, cssClass);
+    });
+  });
 }
 
 function replaceTitle(name, cssClass) {
   var imgTag = "<img src='" + chrome.extension.getURL('images/' + name.toLowerCase() + '.png') + "'/>";
 
-  $(cssClass + ":contains('" + name + "'):visible").each(function() {
-    var new_content = $(this).text()
-                             .replace(name, imgTag);
-    $(this).html(new_content);
+  var elements = $(cssClass + ":containsCaseInsensitive('" + name + "'):visible"); // selector here was changed to be case-insensitive
+
+  $(elements).each(function() {
+    $(this).parents('.story').addClass(name.toLowerCase() + '-story'); // add to all matched stories css class, e.g: quick-story
+
+    var regEx = new RegExp(name, 'i'); // Regular expression to find all substring occurencies; `i` means case-insensitive
+
+    var newContent = $(this).text().replace(regEx, imgTag); // Replace matched substring with img tag
+
+    $(this).html(newContent);
   });
 }
